5792a638233d9798c4379520f92872445ecffa1d
kpta119 <kacper.pt119@gmail.com>
2024-10-27 18:01:54 +0100

    Dodanie kopiowania osobników do nowych populacji zamiast działania na referencjach

--- evolutionaryAlgorithm.py
+++ evolutionaryAlgorithm.py
@@ -1,5 +1,6 @@
 import numpy as np
 import random
+import copy
 from cec2017.functions import f2, f13
 
 
@@ -19,25 +20,24 @@ class Individual:
             
 
 class Population:
-    def __init__(self, size, function):
+    def __init__(self, size, function, individuals=None):
+        if individuals is None:
+            self.individuals = [Individual(10, function) for _ in range(size)]
+        else:
+            self.individuals = individuals
         self.size = size
-        self.individuals = [Individual(10, function) for _ in range(size)]
         self.function = function
     
     
-    def evaluatePopulation(self, population=None):
-        if population is None:
-            population = self.individuals
+    def evaluatePopulation(self):
         evaluation = np.zeros(self.size)
-        for i, ind in enumerate(population):
+        for i, ind in enumerate(self.individuals):
             evaluation[i] = ind.evaluate()
         return evaluation
 
-    def findTheBest(self, evaluation, population=None) -> Individual:
-        if population is None:
-            population = self.individuals
+    def findTheBest(self, evaluation) -> Individual:
         theBestIndex = np.argmin(evaluation)
-        theBest = population[theBestIndex]
+        theBest = self.individuals[theBestIndex]
         return theBest
     
     def reproduction(self): #tournament selection
@@ -45,43 +45,41 @@ class Population:
         for i in range(self.size):
             ind1 = self.individuals[random.randint(0,self.size-1)]
             ind2 = self.individuals[random.randint(0,self.size-1)]
-            if ind1.evaluate() <= ind2.evaluate():
-                Rpopulation.append(ind1)
-            else:
-                Rpopulation.append(ind2)
-        return Rpopulation
+            win = ind1 if ind1.evaluate() <= ind2.evaluate() else ind2
+            Rpopulation.append(copy.deepcopy(win))
+        return Population(self.size, self.function, Rpopulation)
 
-    def mutationPopulation(self, population, sigma):
-        for ind in population:
+    def mutationPopulation(self, sigma):
+        for ind in self.individuals:
             ind.mutation(sigma)
-        return population
+        return self
 
     def succsession(self, newPopulation):
-        self.individuals = newPopulation
+        self.individuals = newPopulation.individuals
 
-def evolutionaryAlgorithm(function, population: Population, sigma: float, tMax: int):
+def evolutionaryAlgorithm(population: Population, sigma: float, tMax: int):
     evaluation = population.evaluatePopulation()
     theBest = population.findTheBest(evaluation)
-    theBestValue = function(theBest.genes)
+    theBestValue = theBest.evaluate()
     for i in range(tMax):
         Rpopulation = population.reproduction()
-        Mpopulation = population.mutationPopulation(Rpopulation, sigma)
-        evaluation2 = population.evaluatePopulation(Mpopulation)
-        theBestCandidate = population.findTheBest(evaluation2, Mpopulation)
-        if function(theBestCandidate.genes) <= theBestValue:
+        Mpopulation = Rpopulation.mutationPopulation(sigma)
+        evaluation2 = Mpopulation.evaluatePopulation() #array ocenionych osobników
+        theBestCandidate = Mpopulation.findTheBest(evaluation2)
+        if theBestCandidate.evaluate() <= theBestValue:
             theBest = theBestCandidate
-            theBestValue = function(theBestCandidate.genes)
+            theBestValue = theBestCandidate.evaluate()
         population.succsession(Mpopulation)
     return theBest.genes, theBestValue
 
 def main():
     BUDGET = 10000
-    SIZE = 20
-    SIGMA = 0.3
+    SIZE = 10
+    SIGMA = 0.5
     function = f2
-    population = Population(SIZE, function)
+    initialPopulation = Population(SIZE, function)
     tMax = int(BUDGET/SIZE)
-    print(evolutionaryAlgorithm(function, population, SIGMA, tMax))
+    print(evolutionaryAlgorithm(initialPopulation, SIGMA, tMax))
 
 
 if __name__ == "__main__":
